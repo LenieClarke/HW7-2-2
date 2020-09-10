954deef3fa2b8bd49f1cd0e6cf5f72b96cb8fe67
Elena Kurbatova <lenie.clarke@mail.ru>
2020-09-10 18:45:31 +0300

    fix permission

--- app/src/main/java/com/example/hw7_2_2/MainActivity.java
+++ app/src/main/java/com/example/hw7_2_2/MainActivity.java
@@ -95,14 +95,18 @@ public class MainActivity extends AppCompatActivity {
                     makeCall();
                 } else {
                     // Разрешение не дано. Закрываем приложение
-                    finish();
+                    //finish();
+                    Toast.makeText(this, getString(R.string.permissionNotReceivedToast),
+                            Toast.LENGTH_SHORT).show();
                 }
             }
             case MY_PERMISSIONS_REQUEST_SEND_SMS: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     sendingSMS();
                 } else {
-                    finish();
+                    //finish();
+                    Toast.makeText(this, getString(R.string.permissionNotReceivedToast),
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         }
